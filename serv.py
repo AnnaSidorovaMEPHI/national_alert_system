8c900ae66a3d20f92979cfaa28b6e4ee9414d0dc
finkrer <finkrer@gmail.com>
2020-08-07 19:21:39 +0300

    Do stuff

--- serv.py
+++ serv.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template, redirect, url_for, session, send_from_directory
 
 app = Flask(__name__)
-
-@app.route('/profile')
-def profile():
-    data = dict(num=request.args.get('num'),
-                chord=request.args.get('chord'))
-    return render_template('profile.html', data=data)
 
 
 @app.route('/')
 def index():
-    num = request.args.get('num')
-    if num:
-        return redirect(url_for('profile', num=int(num), chord='0'))
     return render_template('index.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/cabinet')
+def cabinet():
+    return render_template('cabinet.html')
+
+
+@app.route('/styles/<path:path>')
+def styles(path):
+    return send_from_directory('styles', path)
+
+
+@app.route('/scripts/<path:path>')
+def scripts(path):
+    return send_from_directory('scripts', path)
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=3000, debug=True)
