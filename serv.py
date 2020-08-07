627169b4224299b5cf841e24aa9bcc50346c7282
AnnaSidorovaMEPHI <Anna.Sidorova.1998@yandex.ru>
2020-08-07 19:21:36 +0300

    Добавлена рассылка

--- serv.py
+++ serv.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
+import smtplib
 from flask import Flask, request, render_template, redirect, url_for, session
+from flask_mail import Mail, Message as EmailMessage
+import os
 
+mail = Mail()
 app = Flask(__name__)
+mail.init_app(app)
+
+
 
 @app.route('/profile')
 def profile():
@@ -12,10 +19,16 @@ def profile():
 
 @app.route('/')
 def index():
-    num = request.args.get('num')
-    if num:
-        return redirect(url_for('profile', num=int(num), chord='0'))
-    return render_template('index.html')
+	smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+	smtpObj.starttls()
+	smtpObj.login('archellik@gmail.com','smtp3228')
+	smtpObj.sendmail('archellik@gmail.com',"Anna.Sidorova.1998@yandex.ru","go to bed!")
+	smtpObj.quit()
+
+	num = request.args.get('num')
+	if num:
+		return redirect(url_for('profile', num=int(num), chord='0'))
+	return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=3000, debug=True)
