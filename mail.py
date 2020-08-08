67511877165503cca5cdd6450e88938bbad7edc1
finkrer <finkrer@gmail.com>
2020-08-08 07:00:21 +0300

    Remove mail from index

--- mail.py
+++ mail.py
@@ -4,30 +4,29 @@ import sys
 import json
 import os
 
+
 class EmailSender(object):
-	"""docstring for EmailSender"""
-	def __init__(self, addresses = []):
-		self.smtp = smtplib.SMTP('smtp.gmail.com', 587)
-		self.addresses = addresses
+    """docstring for EmailSender"""
 
-	def send_alert(self, text):
-		if self.addresses:
-			self.smtp.starttls()
-			conf = self._load_json(".\\src\\config.json")
-			login = conf["login"]
-			password = conf["password"]
-			self.smtp.login(login, password)
-			self.smtp.sendmail(login, self.addresses, text)
-			self.smtp.quit()
-		else:
-			sys.stdout.write("Can not send email to" + str(addresses))
+    def __init__(self, addresses=[]):
+        self.smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        self.addresses = addresses
 
-	@staticmethod	
-	def _load_json(path):
-		abs_path = path
-		with open(abs_path, "rb") as json_file:
-			data = json.loads(json_file.read())
-		return data
+    def send_alert(self, text):
+        if self.addresses:
+            self.smtp.starttls()
+            conf = self._load_json("./src/config.json")
+            login = conf["login"]
+            password = conf["password"]
+            self.smtp.login(login, password)
+            self.smtp.sendmail(login, self.addresses, text)
+            self.smtp.quit()
+        else:
+            sys.stdout.write("Can not send email to" + str(addresses))
 
-
-
+    @staticmethod
+    def _load_json(path):
+        abs_path = path
+        with open(abs_path, "rb") as json_file:
+            data = json.loads(json_file.read())
+        return data
